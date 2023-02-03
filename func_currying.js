4432723d3ec291facf95eb998712c5cdb8aa1605
itsjustabhii <itsjustabhii@gmail.com>
2023-02-03 16:19:19 +0530

    solve getSum in one line

--- func_currying.js
+++ func_currying.js
@@ -1,13 +1,18 @@
-const getSum = (a) => {
-  return function (b) {
-    return function (c) {
-      return function (d) {
-        return function (e) {
-          console.log(a + b + c + d + e);
-        };
-      };
-    };
-  };
-};
+// const getSum = (a) => {
+//   return function (b) {
+//     return function (c) {
+//       return function (d) {
+//         return function (e) {
+//           console.log(a + b + c + d + e);
+//         };
+//       };
+//     };
+//   };
+// };
 
-getSum(5)(4)(3)(2)(1);
+// getSum(5)(4)(3)(2)(1);
+
+//To solve in one line
+const getSum = (a) => (b) => (c) => (d) => (e) => a + b + c + d + e;
+const total = getSum(5)(4)(3)(2)(1);
+console.log(total);
