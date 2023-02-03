8152a9c5f0916abfc3035d5c88a75f162160c37e
itsjustabhii <itsjustabhii@gmail.com>
2023-02-03 16:05:19 +0530

    fibonacci and vowels&consonants

---
+++ fib.js
@@ -0,0 +1,9 @@
+const fib = (n) => {
+  const fibArray = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fibArray.push(fibArray[i - 2] + fibArray[i - 1]);
+  }
+  return fibArray;
+};
+console.log(fib(10));
---
+++ vowel_consonant.js
@@ -0,0 +1,18 @@
+/** To find whether a char is vowel or consonants? */
+
+const findVowelOrNot = () => {
+  let char = "b";
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    console.log("Char is a vowel");
+  } else {
+    console.log("Char is a consonant!");
+  }
+};
+
+findVowelOrNot();
