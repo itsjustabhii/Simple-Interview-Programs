99389c4bb04d397f67c9f48fc64a17e711498ff4
itsjustabhii <itsjustabhii@gmail.com>
2023-02-07 11:58:11 +0530

    added Promise definition

--- promises.js
+++ promises.js
@@ -1,3 +1,9 @@
+/*
+Definition - A Promise in JavaScript is an object representing the completion or failure of an asynchronous operation
+ It provides a way to register callbacks to be called when the operation is complete or has failed
+ 
+ */
+
 let p = new Promise((resolve, reject) => {
   let a = 1 + 1;
   if (a === 2) {
